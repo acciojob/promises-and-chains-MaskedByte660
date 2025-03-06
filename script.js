d7742d7eed8b7d020f4ae9f868e25c65c9d7a60c
MaskedByte660 <codingtechinfo01@gmail.com>
2025-03-06 12:03:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",(e)=>{
+	e.preventDefault();//this will prevent reloading of your page
+	const age = document.getElementById("age").value;
+	new Promise((resolve, reject)=>{
+		if(age>18){
+			setTimeout(()=>{
+				resolve("Welcome, . You can vote.")
+			},4000)
+		}
+		else{
+			setTimeout(()=>{
+				reject("Oh sorry . You aren't old enough.")
+			},4000)
+		}
+	}).then((res)=>alert(res))
+		.catch((err)=>alert(err))
+})
